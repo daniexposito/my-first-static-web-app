9b696e4c11f4c42576b702392be1d8a7551e34f1
daniel.exposito@habber.com <daniel.exposito@habber.com>
2025-05-12 16:41:54 +0200

    Initial test commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 
 function App() {
+  // Example of using axios to fetch data
+  React.useEffect(() => {
+    axios.get('https://api.restful-api.dev/objects')
+      .then(response => {
+        console.log(response.data);
+        const value = response.data[0].name;
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
   const value = 'World';
   return <div>Hello {value}</div>;
 }
